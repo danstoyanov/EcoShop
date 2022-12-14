fd5f33cf4fe2037daf45c4b241ce808fe160c664
Dan Stoyanov <Dan Stoyanov@DESKTOP-QUA3KCD>
2022-12-14 16:02:52 +0200

    Add project files.

---
+++ EcoShop.Database/ApplicationDbContext.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace EcoShop.Database
+{
+    public class ApplicationDbContext
+    {
+        public ApplicationDbContext()
+        {
+            
+        }
+    }
+}
