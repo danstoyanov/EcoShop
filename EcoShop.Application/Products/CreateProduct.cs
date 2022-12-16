8af21b3b7a53e283720b645375778e8835942e91
Daniel Stoyanov <dakovich@abv.bg>
2022-12-16 15:30:04 +0200

    🆕 Update: - Add Product to Database. - Create DbContext. - Add References between projects.

--- EcoShop.Application/Products/CreateProduct.cs
+++ EcoShop.Application/Products/CreateProduct.cs
@@ -1,7 +1,25 @@
-﻿namespace EcoShop.Application.Products
+﻿using EcoShop.Database;
+using EcoShop.Domain.Models;
+
+namespace EcoShop.Application.Products
 {
     public class CreateProduct
     {
+        private ApplicationDbContext _context;
 
+        public CreateProduct(ApplicationDbContext context)
+        {
+            _context = context;
+        }
+
+        public void Create(int Id, string Name, string Description)
+        {
+            _context.Products.Add(new Product
+            {
+                Id = Id,
+                Name = Name,
+                Description = Description
+            });
+        }
     }
 }
--- EcoShop.Database/ApplicationDbContext.cs
+++ EcoShop.Database/ApplicationDbContext.cs
@@ -4,13 +4,17 @@ using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 
+using Microsoft.EntityFrameworkCore;
+using Microsoft.AspNetCore.Identity.EntityFrameworkCore;
+
+using EcoShop.Domain.Models;
+
 namespace EcoShop.Database
 {
-    public class ApplicationDbContext
+    public class ApplicationDbContext : IdentityDbContext
     {
-        public ApplicationDbContext()
-        {
-            
-        }
+        public ApplicationDbContext(DbContextOptions options) : base() { }
+
+        public DbSet<Product> Products { get; set; }
     }
 }
